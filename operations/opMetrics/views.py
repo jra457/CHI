b9441f038b8120b3955b24b9cbabeb25412b785f
jra457 <111715907+jra457@users.noreply.github.com>
2022-12-10 13:25:11 -0600

    Update views.py
    Updated views.py file to support the updated packagesperhour.html form, but new form method does not integrate correctly.

--- operations/opMetrics/views.py
+++ operations/opMetrics/views.py
@@ -4,7 +4,7 @@ import calendar
 from calendar import HTMLCalendar
 from .forms import PackagesForm
 from django.views import generic
-from .models import Division, Region, District, Location, \
+from .models import Division, Region, District, Location, Package, \
     packagesPerHour, getLevel2Dict, getToday, getOutputData
 
 # ~~~~~ Add Packages ~~~~~
@@ -53,15 +53,56 @@ def VAS(request):
 # ~~~~~
 
 
-
+from datetime import date
+from datetime import timedelta
+from django.db.models import Sum, Avg
 # ~~~~~ Home ~~~~~
 def home(request):
+    year=datetime.now().year
+    month=datetime.now().strftime('%B')
+    today = getToday()
+    today = date.today()
+    yesterday = today - timedelta(days=1)
+    prev7 = today - timedelta(days=7)
+    prev30 = today - timedelta(days=30)
+    # ~~~~~
 
+    # ~~~~~ Initialize Calendar
+    month = month.title()
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+
+    cal = HTMLCalendar().formatmonth(year, month_number)
+    now = datetime.now()
+    current_year = now.year
+
+    time = now.strftime('%I:%M:%S %p')
+    # ~~~~~
+
+    # ~~~~~ Calculate Sums & Averages ~~~~~
+    todayRange = Package.objects.filter(sender_id=10, dateSent__range=[today, today])
+    testSum = todayRange.count()
+    print(testSum)
+    # todaySum = todayRange.aggregate(Sum('pph'))
+    # todayAvg = todayRange.aggregate(Avg('pph'))
+    print("TODAYRANGE", todayRange)
+    yesterdayRange = packagesPerHour.objects.filter(location_id=21, date__range=[yesterday, yesterday])
+    yesterdaySum = yesterdayRange.aggregate(Sum('pph'))
+    yesterdayAvg = yesterdayRange.aggregate(Avg('pph'))
+
+    weekRange = packagesPerHour.objects.filter(location_id=21, date__range=[prev7, yesterday])
+    weekSum = weekRange.aggregate(Sum('pph'))
+    weekAvg = weekRange.aggregate(Avg('pph'))
+
+    monthRange = packagesPerHour.objects.filter(location_id=21, date__range=[prev30, yesterday])
+    monthSum = monthRange.aggregate(Sum('pph'))
+    monthAvg = monthRange.aggregate(Avg('pph'))
+    # ~~~~~
 
     now = datetime.now()
     todayStr =f"{now.strftime('%b')} {now.strftime('%d')}, {now.strftime('%Y')}"
     # ~~~~~ Return Generated Values ~~~~~
-    today = getToday()
     context = {
         'today': today,
         'todayStr': todayStr,
@@ -122,6 +163,9 @@ def Company(request):
 from django.core.paginator import Paginator
 def packagesperhour(request, level1='company', level2='all', sDatePPH=getToday(), eDatePPH=getToday()):
     if request.method == 'POST':
+        level1 = request.POST.get("level1").lower()
+        # level2 = request.POST.get("level2").lower()
+        print("DROP DOWN IF", level1)
         sDate = request.POST.get("sDatePPH")
         eDate = request.POST.get("eDatePPH")
         sDatePPH = sDate
@@ -133,6 +177,7 @@ def packagesperhour(request, level1='company', level2='all', sDatePPH=getToday()
         dataRange = getOutputData(level1, id, sDatePPH, eDatePPH)
     
     else:
+        print("DROP DOWN ELSE", level1)
         sDatePPH = sDatePPH
         eDatePPH = eDatePPH
 
@@ -140,8 +185,8 @@ def packagesperhour(request, level1='company', level2='all', sDatePPH=getToday()
         level2List = list(level2Dict.keys())
         id = level2Dict.get(level2)
         dataRange = getOutputData(level1, id, sDatePPH, eDatePPH)
-    testNum = request.POST.get("test")
 
+    
     paginator = Paginator(dataRange, 50)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -152,15 +197,16 @@ def packagesperhour(request, level1='company', level2='all', sDatePPH=getToday()
             rowNumber = 0
         rowNumber = rowNumber + 1
         return rowNumber
-        
+    
     # ~~~~~ Return Generated Values ~~~~~
     today = getToday()
+    print("level1 context", level1)
     context = {
         'dataRange': dataRange,
         'sDatePPH': sDatePPH,
         'eDatePPH': eDatePPH,
 
-        'level1': level1,
+        'level1': level1.title(),
         'level2': level2,
         'level2List': level2List,
 
